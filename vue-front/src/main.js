a01af2b4ffc6c06d7a03a854399851689d8910c8
ArielMAJ <ariel.maj@hotmail.com>
2023-12-20 16:58:27 -0300

    WIP: Vue+FastAPI+mocked integration with GH API.

--- vue-front/src/main.js
+++ vue-front/src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
-createApp(App).mount("#app");
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+createApp(App).use(vuetify).mount("#app");
